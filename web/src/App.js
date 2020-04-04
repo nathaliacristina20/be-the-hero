de2b9c4e484027286e9c3c58869ac672b1630473
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-04-04 11:24:20 -0300

    Clear aplication

--- web/src/App.js
+++ web/src/App.js
@@ -1,22 +1,8 @@
 import React from 'react';
-import logo from './assets/rocket.svg';
-import astronaut from './assets/astronaut.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <img className="logo" src={logo} alt="Rocketseat Logo" />
-      <h2>Welcome to OmniStack!</h2>
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn React
-      </a>
-      <img className="astronaut" src={astronaut} alt="Astronaut" />
-    </div>
-  );
+  return <div className="App" />;
 }
 
 export default App;
